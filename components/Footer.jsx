42778d365af03b48f16bce26954730d16283530a
Marcodellavecchia95 <mdellavecchia95@gmail.com>
2025-05-20 15:58:40 +0200

    add upper footer

---
+++ components/Footer.jsx
@@ -0,0 +1,129 @@
+export default function Footer() {
+  return (
+    <footer id="page-footer">
+      <div className="upper-footer">
+        <div className="container">
+          <div className="row">
+            <div className="right-col">
+              <div className="col-33">
+                <div className="card">
+                  <div>
+                    <h3>DC COMICS</h3>
+                    <ul>
+                      <li>
+                        <a href="#">Characters</a>
+                      </li>
+                      <li>
+                        <a href="#">Comics</a>
+                      </li>
+                      <li>
+                        <a href="#">Movies</a>
+                      </li>
+                      <li>
+                        <a href="#">Tv</a>
+                      </li>
+                      <li>
+                        <a href="#">Games</a>
+                      </li>
+                      <li>
+                        <a href="#">Videos</a>
+                      </li>
+                      <li>
+                        <a href="#">News</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="card">
+                  <div>
+                    <h3 className="shop-list-h3">SHOP</h3>
+                    <ul>
+                      <li>
+                        <a href="#">Shop DC</a>
+                      </li>
+                      <li>
+                        <a href="#">Shop DC Collectibles</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div className="col-33">
+                <div className="card">
+                  <div>
+                    <h3>DC</h3>
+                    <ul>
+                      <li>
+                        <a href="#">Terms Of Use</a>
+                      </li>
+                      <li>
+                        <a href="#">Privacy Policy (New)</a>
+                      </li>
+                      <li>
+                        <a href="#">Ad Choices</a>
+                      </li>
+                      <li>
+                        <a href="#">Advertising</a>
+                      </li>
+                      <li>
+                        <a href="#">Jobs</a>
+                      </li>
+                      <li>
+                        <a href="#">Subscriptions</a>
+                      </li>
+                      <li>
+                        <a href="#">Talent Workshops</a>
+                      </li>
+                      <li>
+                        <a href="#">CPSC Certificates</a>
+                      </li>
+                      <li>
+                        <a href="#">Ratings</a>
+                      </li>
+                      <li>
+                        <a href="#">Shop Help</a>
+                      </li>
+                      <li>
+                        <a href="#">Contact Us</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div className="col-33">
+                <div className="card">
+                  <div>
+                    <h3>SITES</h3>
+                    <ul>
+                      <li>
+                        <a href="#">DC</a>
+                      </li>
+                      <li>
+                        <a href="#">MAD Magazine</a>
+                      </li>
+                      <li>
+                        <a href="#">DC Kids</a>
+                      </li>
+                      <li>
+                        <a href="#">DC Universe</a>
+                      </li>
+                      <li>
+                        <a href="#">DC Power Visa</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="left-col">
+              <figure>
+                <img src="src/assets/img/dc-logo-bg.png" alt="" />
+              </figure>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bottom-footer"></div>
+    </footer>
+  );
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./assets/index.css";
 import Header from "../components/Header";
 import Main from "../components/Main";
+import Footer from "../components/Footer";
 
 export default function App() {
   return (
     <>
       <Header />
       <Main />
+      <Footer />
     </>
   );
 }
