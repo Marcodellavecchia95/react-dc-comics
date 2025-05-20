2d3a9fe2308b74bd27373c7f02009a155cd97368
Marcodellavecchia95 <mdellavecchia95@gmail.com>
2025-05-20 14:28:42 +0200

    add header

---
+++ components/Header.jsx
@@ -0,0 +1,47 @@
+export default function Header() {
+  return (
+    <header id="page-header">
+      <div className="container">
+        <figure>
+          <img src="src/assets/img/dc-logo.png" alt="Logo DC" />
+        </figure>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">CHARACTERS</a>
+            </li>
+            <li>
+              <a className="active" href="#">
+                COMICS
+              </a>
+            </li>
+            <li>
+              <a href="#">MOVIES</a>
+            </li>
+            <li>
+              <a href="#">TV</a>
+            </li>
+            <li>
+              <a href="#">GAMES</a>
+            </li>
+            <li>
+              <a href="#">COLLECTIBLES</a>
+            </li>
+            <li>
+              <a href="#">VIDEOS</a>
+            </li>
+            <li>
+              <a href="#">FANS</a>
+            </li>
+            <li>
+              <a href="#">NEWS</a>
+            </li>
+            <li>
+              <a href="#">SHOP</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
